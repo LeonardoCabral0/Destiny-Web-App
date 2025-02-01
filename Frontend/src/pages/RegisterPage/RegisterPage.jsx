f760c89ca8f8ba33bb6752eae2b31279d6b2371d
LeonardoCabral0 <143377556+LeonardoCabral0@users.noreply.github.com>
2025-02-01 14:20:26 -0300

    feat: Adicionado estrutura e estilização da página que registra pontos turísticos

--- Frontend/src/pages/RegisterPage/RegisterPage.jsx
+++ Frontend/src/pages/RegisterPage/RegisterPage.jsx
@@ -1,8 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './styles.module.css'
 
 export const RegisterPage = () => {
-  return (
-    <div>RegisterPage</div>
-  )
+    const [selectedState, setSelectedState] = useState("");
+
+    return (
+      <section className={styles.wrapper}>
+        <form className={styles.form}>
+          <h2>Cadastre um novo ponto turístico!</h2>
+          <section className={styles.containerInputName}>
+            <label htmlFor='name'>Name:</label>
+            <input className={styles.inputName} id="name" type="text" placeholder='Digite o nome do ponto turístico' />
+          </section>
+          <section className={styles.containerLocalization}>
+            <label htmlFor='cidade'>Localizacação:</label>
+            <section>
+              <label htmlFor='cidade'>UF/Cidade:</label>
+              <section className={styles.containerAdress}>
+                <select
+                  id="estados"
+                  value={selectedState}
+                  onChange={(e) => setSelectedState(e.target.value)}
+                  className={styles.containerStates}
+                >
+                  <option value=""></option>
+                  {statesBrazil.map((state) => (
+                    <option key={state.sigla} value={state.sigla}>
+                      {state.nome}
+                    </option>
+                  ))}
+                </select>
+                <input className={styles.inputCity} id="cidade" type="text" />
+              </section>
+  
+            </section>
+            <label htmlFor='cidade'>Localizacação:</label>
+            <input className={styles.inputReferency} id="referencia" type="text" />
+          </section>
+          <section className={styles.containerDescription}>
+            <label htmlFor='descricao'>Referência:</label>
+            <textarea id="descricao"/>
+          </section>
+          <button className={styles.buttonForm}>Cadastre</button>
+        </form>
+      </section>
+    )
 }
