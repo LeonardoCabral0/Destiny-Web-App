ae6465c71c7159d659035fb4f2364735da8eb789
LeonardoCabral0 <143377556+LeonardoCabral0@users.noreply.github.com>
2025-02-01 14:17:19 -0300

    feat: Configurar React Router Dom com o caminho das páginas
    Adicionado o caminho das páginas e menu de navegação padrão para todas elas

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,33 @@
 import './App.css'
+import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom'
+
+import { MenuBar } from './components/MenuBar/MenuBar'
+import { HomePage } from './pages/HomePage/HomePage'
+import { RegisterPage } from './pages/RegisterPage/RegisterPage'
+import { ExplorerPage } from './pages/ExplorePage/ExplorerPage'
+
+function Layout() {
+  return (
+    <>
+      <MenuBar />
+      <Outlet />
+    </>
+  )
+}
+
+const router = createBrowserRouter([
+  {
+    element: <Layout />,
+    children: [
+      { path: "/", element: <HomePage /> },
+      { path: "/cadastro", element: <RegisterPage /> },
+      { path: "/explorar", element: <ExplorerPage />}
+    ],
+  },
+]);
 
 function App() {
-
-  return (
-    <div>Hello World</div>
-  )
+  return <RouterProvider router={router} />
 }
 
 export default App
