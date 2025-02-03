72a9e9933830f6e98ea73c2e36ecea58d0eea0ad
LeonardoCabral0 <143377556+LeonardoCabral0@users.noreply.github.com>
2025-02-03 05:57:56 -0300

    feat: Adicionar nova migrations para popular a tabela de pontos turísiticos

--- Backend/TouristSpot/TouristSpot.Infrastructure/APIDbContext.cs
+++ Backend/TouristSpot/TouristSpot.Infrastructure/APIDbContext.cs
@@ -13,6 +13,48 @@ namespace TouristSpot.Infrastructure
             modelBuilder.ApplyConfigurationsFromAssembly(typeof(APIDbContext).Assembly);
 
             base.OnModelCreating(modelBuilder);
+
+            var touristSpots = new Domain.Entities.TouristSpot[20];
+            var startDate = new DateTime(2022, 01, 01);
+
+            var touristSpotsData = new (string Name, string Description, string Localization, string City, string State)[]
+            {
+            ("Cristo Redentor", "Estátua gigante de Jesus Cristo, localizada no Rio de Janeiro.", "Parque Nacional da Tijuca", "Rio de Janeiro", "RJ"),
+            ("Iguazu Falls", "Cataratas do Iguaçu, uma das maiores quedas d'água do mundo.", "Parque Nacional do Iguaçu", "Foz do Iguaçu", "PR"),
+            ("Copacabana Beach", "Praia mundialmente famosa no Rio de Janeiro.", "Avenida Atlântica", "Rio de Janeiro", "RJ"),
+            ("Pão de Açúcar", "Montanha famosa do Rio de Janeiro com vista panorâmica.", "Urca", "Rio de Janeiro", "RJ"),
+            ("Amazônia", "Floresta tropical famosa pela biodiversidade.", "Amazônia", "Manaus", "AM"),
+            ("Pantanal", "A maior área úmida do mundo, rica em fauna e flora.", "Pantanal Mato-Grossense", "Cuiabá", "MT"),
+            ("Pelourinho", "Centro histórico de Salvador, conhecido por sua arquitetura colonial.", "Centro Histórico", "Salvador", "BA"),
+            ("Catedral da Sé", "Catedral gótica imponente localizada em São Paulo.", "Praça da Sé", "São Paulo", "SP"),
+            ("Teatro Amazonas", "Teatro histórico situado no coração de Manaus.", "Centro de Manaus", "Manaus", "AM"),
+            ("Mercado Municipal", "Mercado tradicional em São Paulo, com produtos típicos.", "Rua da Cantareira", "São Paulo", "SP"),
+            ("Lagoa Rodrigo de Freitas", "Lagoa famosa no Rio de Janeiro, rodeada por montanhas.", "Zona Sul", "Rio de Janeiro", "RJ"),
+            ("Museu Nacional", "Museu de História Natural e Antropologia no Rio de Janeiro.", "Quinta da Boa Vista", "Rio de Janeiro", "RJ"),
+            ("Centro Histórico de Ouro Preto", "Cidade histórica conhecida por sua arquitetura colonial.", "Ouro Preto",  "Ouro Preto","MG"),
+            ("Praia do Forte", "Praia paradisíaca e ponto de observação de tartarugas.", "Costa do Sauípe", "Camaçari", "BA"),
+            ("Serrinha do Alambari", "Reserva natural em Penedo, com cachoeiras e trilhas.", "Alambari", "Itatiaia", "RJ"),
+            ("Parque Nacional de Jericoacoara", "Área de conservação no Ceará com belas praias e dunas.", "Jericoacoara", "Ceará", "CE"),
+            ("Cataratas do Iguaçu", "Impressionante conjunto de quedas d'água na fronteira entre Brasil e Argentina.", "Parque Nacional do Iguaçu", "Foz do Iguaçu", "PR"),
+            ("Ilha do Cardoso", "Ilha paradisíaca e parque natural no litoral paulista.", "Ilha Cardoso", "Cananéia", "SP"),
+            ("São Jorge", "Vila no Parque Nacional da Chapada dos Veadeiros, famosa por suas trilhas.", "Chapada dos Veadeiros", "Chapada dos Veadeiros", "GO"),
+            ("Ilha Grande", "Ilha famosa por suas praias e biodiversidade.", "Angra dos Reis", "Angra dos Reis","RJ")
+            };
+
+            for (int i = 0; i < touristSpotsData.Length; i++)
+            {
+                touristSpots[i] = new Domain.Entities.TouristSpot(
+                    i + 1,
+                    touristSpotsData[i].Name,
+                    touristSpotsData[i].Description,
+                    touristSpotsData[i].Localization,
+                    touristSpotsData[i].City,
+                    touristSpotsData[i].State,
+                    startDate.AddDays(i)
+                );
+            }
+
+            modelBuilder.Entity<Domain.Entities.TouristSpot>().HasData(touristSpots);
         }
     }
 }
---
+++ Backend/TouristSpot/TouristSpot.Infrastructure/Migrations/20250203085517_SeedTouristSpotsData.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace TouristSpot.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedTouristSpotsData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "TouristSpots",
+                columns: new[] { "Id", "City", "CreatedDate", "Description", "Localization", "Name", "State" },
+                values: new object[,]
+                {
+                    { 1, "Rio de Janeiro", new DateTime(2022, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Estátua gigante de Jesus Cristo, localizada no Rio de Janeiro.", "Parque Nacional da Tijuca", "Cristo Redentor", "RJ" },
+                    { 2, "Foz do Iguaçu", new DateTime(2022, 1, 2, 0, 0, 0, 0, DateTimeKind.Unspecified), "Cataratas do Iguaçu, uma das maiores quedas d'água do mundo.", "Parque Nacional do Iguaçu", "Iguazu Falls", "PR" },
+                    { 3, "Rio de Janeiro", new DateTime(2022, 1, 3, 0, 0, 0, 0, DateTimeKind.Unspecified), "Praia mundialmente famosa no Rio de Janeiro.", "Avenida Atlântica", "Copacabana Beach", "RJ" },
+                    { 4, "Rio de Janeiro", new DateTime(2022, 1, 4, 0, 0, 0, 0, DateTimeKind.Unspecified), "Montanha famosa do Rio de Janeiro com vista panorâmica.", "Urca", "Pão de Açúcar", "RJ" },
+                    { 5, "Manaus", new DateTime(2022, 1, 5, 0, 0, 0, 0, DateTimeKind.Unspecified), "Floresta tropical famosa pela biodiversidade.", "Amazônia", "Amazônia", "AM" },
+                    { 6, "Cuiabá", new DateTime(2022, 1, 6, 0, 0, 0, 0, DateTimeKind.Unspecified), "A maior área úmida do mundo, rica em fauna e flora.", "Pantanal Mato-Grossense", "Pantanal", "MT" },
+                    { 7, "Salvador", new DateTime(2022, 1, 7, 0, 0, 0, 0, DateTimeKind.Unspecified), "Centro histórico de Salvador, conhecido por sua arquitetura colonial.", "Centro Histórico", "Pelourinho", "BA" },
+                    { 8, "São Paulo", new DateTime(2022, 1, 8, 0, 0, 0, 0, DateTimeKind.Unspecified), "Catedral gótica imponente localizada em São Paulo.", "Praça da Sé", "Catedral da Sé", "SP" },
+                    { 9, "Manaus", new DateTime(2022, 1, 9, 0, 0, 0, 0, DateTimeKind.Unspecified), "Teatro histórico situado no coração de Manaus.", "Centro de Manaus", "Teatro Amazonas", "AM" },
+                    { 10, "São Paulo", new DateTime(2022, 1, 10, 0, 0, 0, 0, DateTimeKind.Unspecified), "Mercado tradicional em São Paulo, com produtos típicos.", "Rua da Cantareira", "Mercado Municipal", "SP" },
+                    { 11, "Rio de Janeiro", new DateTime(2022, 1, 11, 0, 0, 0, 0, DateTimeKind.Unspecified), "Lagoa famosa no Rio de Janeiro, rodeada por montanhas.", "Zona Sul", "Lagoa Rodrigo de Freitas", "RJ" },
+                    { 12, "Rio de Janeiro", new DateTime(2022, 1, 12, 0, 0, 0, 0, DateTimeKind.Unspecified), "Museu de História Natural e Antropologia no Rio de Janeiro.", "Quinta da Boa Vista", "Museu Nacional", "RJ" },
+                    { 13, "Ouro Preto", new DateTime(2022, 1, 13, 0, 0, 0, 0, DateTimeKind.Unspecified), "Cidade histórica conhecida por sua arquitetura colonial.", "Ouro Preto", "Centro Histórico de Ouro Preto", "MG" },
+                    { 14, "Camaçari", new DateTime(2022, 1, 14, 0, 0, 0, 0, DateTimeKind.Unspecified), "Praia paradisíaca e ponto de observação de tartarugas.", "Costa do Sauípe", "Praia do Forte", "BA" },
+                    { 15, "Itatiaia", new DateTime(2022, 1, 15, 0, 0, 0, 0, DateTimeKind.Unspecified), "Reserva natural em Penedo, com cachoeiras e trilhas.", "Alambari", "Serrinha do Alambari", "RJ" },
+                    { 16, "Ceará", new DateTime(2022, 1, 16, 0, 0, 0, 0, DateTimeKind.Unspecified), "Área de conservação no Ceará com belas praias e dunas.", "Jericoacoara", "Parque Nacional de Jericoacoara", "CE" },
+                    { 17, "Foz do Iguaçu", new DateTime(2022, 1, 17, 0, 0, 0, 0, DateTimeKind.Unspecified), "Impressionante conjunto de quedas d'água na fronteira entre Brasil e Argentina.", "Parque Nacional do Iguaçu", "Cataratas do Iguaçu", "PR" },
+                    { 18, "Cananéia", new DateTime(2022, 1, 18, 0, 0, 0, 0, DateTimeKind.Unspecified), "Ilha paradisíaca e parque natural no litoral paulista.", "Ilha Cardoso", "Ilha do Cardoso", "SP" },
+                    { 19, "Chapada dos Veadeiros", new DateTime(2022, 1, 19, 0, 0, 0, 0, DateTimeKind.Unspecified), "Vila no Parque Nacional da Chapada dos Veadeiros, famosa por suas trilhas.", "Chapada dos Veadeiros", "São Jorge", "GO" },
+                    { 20, "Angra dos Reis", new DateTime(2022, 1, 20, 0, 0, 0, 0, DateTimeKind.Unspecified), "Ilha famosa por suas praias e biodiversidade.", "Angra dos Reis", "Ilha Grande", "RJ" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "TouristSpots",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "TouristSpots",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "TouristSpots",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "TouristSpots",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "TouristSpots",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "TouristSpots",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "TouristSpots",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "TouristSpots",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "TouristSpots",
+                keyColumn: "Id",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "TouristSpots",
+                keyColumn: "Id",
+                keyValue: 10);
+
+            migrationBuilder.DeleteData(
+                table: "TouristSpots",
+                keyColumn: "Id",
+                keyValue: 11);
+
+            migrationBuilder.DeleteData(
+                table: "TouristSpots",
+                keyColumn: "Id",
+                keyValue: 12);
+
+            migrationBuilder.DeleteData(
+                table: "TouristSpots",
+                keyColumn: "Id",
+                keyValue: 13);
+
+            migrationBuilder.DeleteData(
+                table: "TouristSpots",
+                keyColumn: "Id",
+                keyValue: 14);
+
+            migrationBuilder.DeleteData(
+                table: "TouristSpots",
+                keyColumn: "Id",
+                keyValue: 15);
+
+            migrationBuilder.DeleteData(
+                table: "TouristSpots",
+                keyColumn: "Id",
+                keyValue: 16);
+
+            migrationBuilder.DeleteData(
+                table: "TouristSpots",
+                keyColumn: "Id",
+                keyValue: 17);
+
+            migrationBuilder.DeleteData(
+                table: "TouristSpots",
+                keyColumn: "Id",
+                keyValue: 18);
+
+            migrationBuilder.DeleteData(
+                table: "TouristSpots",
+                keyColumn: "Id",
+                keyValue: 19);
+
+            migrationBuilder.DeleteData(
+                table: "TouristSpots",
+                keyColumn: "Id",
+                keyValue: 20);
+        }
+    }
+}
