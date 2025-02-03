3517ebd4be2306e2e7b83a94cb48d9fc599d2ca0
LeonardoCabral0 <143377556+LeonardoCabral0@users.noreply.github.com>
2025-02-03 00:06:25 -0300

    feat: Adicionar Home Page

--- Frontend/src/pages/HomePage/HomePage.jsx
+++ Frontend/src/pages/HomePage/HomePage.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
 import styles from './styles.module.css'
+import { Link } from 'react-router-dom'
 
 export const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div className={styles.wrapper}>
+      <div>
+        <h2 className={styles.title}>Explore os Melhores Pontos Turísticos</h2>
+        <p className={styles.subTitle}>Este site é uma plataforma experimental onde você pode explorar pontos turísticos de diferentes lugares. Veja como as informações sobre cada destino podem ser apresentadas de maneira simples e interativa.</p>
+        <div className={styles.containerButtons}>
+          <Link className={`${styles.button} ${styles.ligth}`} to="/cadastro">Cadastrar</Link>
+          <Link className={`${styles.button} ${styles.dark}`} to="/explorar">Explorar</Link>
+        </div>
+      </div>
+    </div>
   )
 }
